7edaa73fe52f02723cffa0af280ddffe25cfa58a
devCurti <joaocurti2002@gmail.com>
2023-07-24 10:43:02 -0300

    Adicionado classe department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,48 +1,12 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import db.DB;
+import model.entities.department;
 
 public class Program {
-
-	public static void main(String[] args) {
-		Connection conn = null;
-		PreparedStatement st = null;
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	public static void main(String args[]) {
 		
-		try {
-			conn = DB.getConnection();
-			
-			st = conn.prepareStatement(
-					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+ " VALUES (?,?,?,?,?)"
-					);
-			
-			st.setString(1, "João Vinícius");
-			st.setString(2, "joaocurti2002@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("01/10/2002").getTime()));
-			st.setDouble(4, 3000);
-			st.setInt(5, 4);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! - Rows Affected: " + rowsAffected);
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		catch(ParseException e) {
-			e.printStackTrace();
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		department obj = new department(1,"Books");
+		
+		System.out.println(obj);
 	}
-
 }
---
+++ src/model/entities/department.java
@@ -0,0 +1,58 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class department implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	
+	public department() {};
+	
+	public department(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		department other = (department) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "department [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+}
